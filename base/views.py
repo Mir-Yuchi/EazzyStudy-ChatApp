1139a5c466ffb6a591ec7710f6d621fb7f109cab
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-07-07 13:44:04 +0500

    added edit option

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -25,5 +26,23 @@ def room(request, pk):
 
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room_update = Room.objects.get(id=pk)
+    form = RoomForm(instance=room_update)
+    context = {'form': form}
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room_update)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     return render(request, 'base/room_form.html', context)
