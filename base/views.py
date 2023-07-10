deddfe691562c510c63b4788c47bfc3d063288d5
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-07-10 19:53:23 +0500

    number of topics is limited up to 5

--- base/views.py
+++ base/views.py
@@ -65,7 +65,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q))
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
