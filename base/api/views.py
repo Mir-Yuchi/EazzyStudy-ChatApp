8285a4f01c025599e5f091ba7ceee66203154eb3
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-07-10 21:37:55 +0500

    error fixed

--- base/api/views.py
+++ base/api/views.py
@@ -2,9 +2,9 @@ from django.http import JsonResponse
 
 
 def getRoutes(request):
-    routes = {
+    routes = [
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
-    }
+    ]
     return JsonResponse(routes, safe=False)
